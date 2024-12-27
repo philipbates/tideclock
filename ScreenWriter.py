10ce75ba779bb84fce5b46b8742c4c3300084a9a
philipbates <48955820+philipbates@users.noreply.github.com>
2024-12-27 01:37:22 +0100

    error image and function

--- ScreenWriter.py
+++ ScreenWriter.py
@@ -54,29 +54,30 @@ def write_to_screen(image):
 
 
 # define function for displaying error
-def display_error(error_source):
-    # Display an error
-    print('Error in the', error_source, 'request.')
-    # Initialize drawing
-    error_image = Image.new('1', (epd.width, epd.height), 255)
-    # Initialize the drawing
-    draw = ImageDraw.Draw(error_image)
-    # draw.text((100, 150), error_source +' ERROR', font=font50, fill=black)
-    # Save the error image
-    error_image_file = 'wave.png'
-    error_image.save(error_image_file)
-    # Close error image
-    error_image.close()
-    # Write error to screen
-    write_to_screen(error_image_file, 30)
+def display_error():
+    print('Writing to screen.') # for debugging
+    # Create new blank image template matching screen resolution
+    h_image = Image.new('1', (epd.width, epd.height), 255)   
+    picfile = r"wave.png"
+    image = Image.open(picfile)
+    print('Screen output file opened.') # for debugging
+    # Initialize the drawing context with template as background
+    #ensure the image is the same size as the screen
+    # image = image.resize((epd.width, epd.height))
+    h_image.paste(image, (0, 0))
+    print('Screen output file pasted.') # for debugging
+    epd.display(epd.getbuffer(h_image))
+    print('Screen output file displayed.') # for debugging
+    # Sleep
+    epd.sleep() # Put screen to sleep to prevent damage
+    # print('Sleeping for ' + str(sleep_seconds) +'.')
+    # time.sleep(sleep_seconds) # Determines refresh rate on data
+    # epd.init() # Re-Initialize screen
+    # print('Screen re-initialized.') # for debugging
 
 
 
 if __name__ == "__main__":
     print('Running main function.')
-    try:
-        # Example usage of write_to_screen function
-        write_to_screen(None)
-    except Exception as e:
-        print('Error in displaying picture function.')
+    display_error()
 
